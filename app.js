0dc14130743a8473422d6c1c1d02839891737b00
LeviEyal <happyeyal@gmail.com>
2022-02-28 21:57:02 +0200

    q

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ app.use(morgan("dev"));
 
 /* Routes */
 app
-  // .get("/api", (req, res) => res.sendFile(`${__dirname}/API.html`))
-  .get("/api", (req, res) => res.send("Hello World"))
+  .get("/api", (req, res) => res.sendFile(`${__dirname}/API.html`))
   .post("/api/purchase", controller.purchase)
   .post("/api/saleDetails", controller.saleDetails)
   .get("/api/books", controller.getBooks)
